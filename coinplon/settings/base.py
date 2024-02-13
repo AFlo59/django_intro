17de60c7a19d203467ab6ba7e8eceab724bb227e
AFlo59 <etudessup59230@gmail.com>
2024-02-13 20:48:04 +0100

    up

--- coinplon/settings/base.py
+++ coinplon/settings/base.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+import os
+import dotenv
+
+dotenv.read_dotenv('.env')
+
+AUTH_USER_MODEL = "main.User"
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*0-h11cj(7%my(^m4ft(w_g=$l53bvim$i=+@y4o64eh^ic!*'
+SECRET_KEY = os.getenv('SECRET_KEY')
+print('Ceci est la secret key ___',SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ('DEBUG')
+print(type(DEBUG))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
